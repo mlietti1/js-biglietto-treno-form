2ceca2cfb9451f94c8a05f7fc2ec4c66313d8b4b
mlietti1 <mcf.lietti@outlook.com>
2022-10-13 10:19:56 +0200

    prima correzione

--- src/js/script.js
+++ src/js/script.js
@@ -1,15 +1,21 @@
+const prezzoKm = 0.21;
+const scontoMinorenni = 0.2;
+const scontoOver = 0.4;
+let prezzoFinale;
+
+let hiddenEl;
+hiddenEl = document.querySelector('.ticket');
 const btnInput = document.getElementById('genera');
+
 btnInput.addEventListener('click', function(){
     const name = document.getElementById('name').value;
     document.getElementById('output-name').innerHTML = name;
-    const prezzoKm = 0.21;
-    const scontoMinorenni = 0.2;
-    const scontoOver = 0.4;
-    let prezzoFinale;
     const kmPercorsi = document.getElementById('km-percorsi').value;
+    hiddenEl.classList.remove('d-hide');
+    
 
     const prezzoIniziale = kmPercorsi * prezzoKm;
-    let etaUtente = document.getElementById('fascia-eta').value;
+    const etaUtente = document.getElementById('fascia-eta').value;
 
     if (etaUtente == "under18") {
     prezzoFinale = prezzoIniziale - prezzoIniziale * scontoMinorenni;
@@ -21,10 +27,11 @@ btnInput.addEventListener('click', function(){
     document.getElementById('output-price').innerHTML = prezzoFinale.toFixed(2) + ` €`;
     document.getElementById('n-carrozza').innerHTML = Math.floor(Math.random() * 8) + 1;
     document.getElementById('offerta').innerHTML = etaUtente;
-    document.getElementById('code').innerHTML = Math.floor(Math.random() * 9999999) + 1;
+    document.getElementById('code').innerHTML = Math.floor(Math.random() * (999999 - 100000) +1 ) + 100000;
 });
 
 const btnReset = document.getElementById('annulla');
+
 btnReset.addEventListener('click', function(){
     document.getElementById('name').value = '';
     document.getElementById('output-name').innerHTML = '';
@@ -33,4 +40,5 @@ btnReset.addEventListener('click', function(){
     document.getElementById('n-carrozza').innerHTML = '';
     document.getElementById('offerta').innerHTML = '';
     document.getElementById('code').innerHTML = '';
+    hiddenEl.classList.add('d-hide');
 });
