172184a404b982e290893be97a08b93269f026b0
mlietti1 <mcf.lietti@outlook.com>
2022-10-13 10:46:15 +0200

    ultima correzione

--- src/js/script.js
+++ src/js/script.js
@@ -2,16 +2,20 @@ const prezzoKm = 0.21;
 const scontoMinorenni = 0.2;
 const scontoOver = 0.4;
 let prezzoFinale;
+let offerta;
+let nameUser;
+const ticketBox = document.querySelector('.ticket');
+const ticketTitle = document.querySelector('.ticket-title');
 
-let hiddenEl;
-hiddenEl = document.querySelector('.ticket');
 const btnInput = document.getElementById('genera');
 
 btnInput.addEventListener('click', function(){
-    const name = document.getElementById('name').value;
-    document.getElementById('output-name').innerHTML = name;
+    nameUser = document.getElementById('name').value;
+    document.getElementById('output-name').innerHTML = nameUser;
     const kmPercorsi = document.getElementById('km-percorsi').value;
-    hiddenEl.classList.remove('d-hide');
+    
+    ticketBox.classList.remove('hide');
+    ticketTitle.classList.remove('hide');
     
 
     const prezzoIniziale = kmPercorsi * prezzoKm;
@@ -19,14 +23,17 @@ btnInput.addEventListener('click', function(){
 
     if (etaUtente == "under18") {
     prezzoFinale = prezzoIniziale - prezzoIniziale * scontoMinorenni;
+    offerta = 'Sconto Junior';
     }else if (etaUtente == "over65") {
     prezzoFinale = prezzoIniziale - prezzoIniziale * scontoOver;
+    offerta = 'Sconto Senior';
     }else {
     prezzoFinale = prezzoIniziale;
+    offerta = 'Prezzo pieno';
     };
     document.getElementById('output-price').innerHTML = prezzoFinale.toFixed(2) + ` €`;
     document.getElementById('n-carrozza').innerHTML = Math.floor(Math.random() * 8) + 1;
-    document.getElementById('offerta').innerHTML = etaUtente;
+    document.getElementById('offerta').innerHTML = offerta;
     document.getElementById('code').innerHTML = Math.floor(Math.random() * (999999 - 100000) +1 ) + 100000;
 });
 
@@ -40,5 +47,6 @@ btnReset.addEventListener('click', function(){
     document.getElementById('n-carrozza').innerHTML = '';
     document.getElementById('offerta').innerHTML = '';
     document.getElementById('code').innerHTML = '';
-    hiddenEl.classList.add('d-hide');
+    ticketBox.classList.add('hide');
+    ticketTitle.classList.add('hide');
 });
